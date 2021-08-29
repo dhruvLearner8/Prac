84815c6c22845ce66ec31cc44a272fe2f9e3e386
Your Name <dhruvmpatel096@gmail.com>
2021-08-29 20:27:17 +0530

    10th

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,11 @@
+
+
+# Register your models here.
 from django.contrib import admin
+from .models import Comp_detail  , Desc
+
+admin.site.register(Comp_detail)
+#admin.site.register(Expense)
+admin.site.register(Desc)
 
 # Register your models here.
